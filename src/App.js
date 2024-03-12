976a1b2044f06f17f8b4eff7470cce1834c79698
Jimmy Jamieson <jjamieson1152@gmail.com>
2024-03-11 21:16:31 -0400

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets (1)/img/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='Nucamp Logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
     </div>
   );
 }
