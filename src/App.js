13e5cdc18d2d9067231b765db0995979a52a5212
Jimmy Jamieson <jjamieson1152@gmail.com>
2024-03-12 20:07:31 -0400

    CampsiteCard and CampsitesList Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets (1)/img/logo.png';
-import './App.css';
+import React from "react";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "./app/assets/img/logo.png";
+import "./App.css";
+import CampsitesList from "./features/campsites/CampsitesList";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
+      <Navbar dark color="primary" sticky="top" expand="md">
         <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='Nucamp Logo' />
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="Nucamp Logo" />
           </NavbarBrand>
         </Container>
       </Navbar>
+      <CampsitesList />
     </div>
   );
 }
