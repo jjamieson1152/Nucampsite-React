e52e267e5f2b8a7dc447ebe6d83bdad490358df9
Jimmy Jamieson <jjamieson1152@gmail.com>
2024-04-09 22:37:20 -0400

    Redux for All Slices

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,9 +1,10 @@
 import { Col } from "reactstrap";
 import { selectAllPartners } from "./partnersSlice";
 import Partner from "./Partner";
+import { useSelector } from "react-redux";
 
 const PartnersList = () => {
-  const partners = selectAllPartners();
+  const partners = useSelector(selectAllPartners);
 
   return (
     <Col className="mt-4">
