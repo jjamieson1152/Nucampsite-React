bf9a3f8a3c9a49cce891433b4dd44643befe93cd
Jimmy Jamieson <jjamieson1152@gmail.com>
2024-04-14 22:38:51 -0400

    User Login Form Validation

--- src/utils/validateContactForm.js
+++ src/utils/validateContactForm.js
@@ -4,7 +4,7 @@ export const validateContactForm = (values) => {
   if (!values.firstName) {
     errors.firstName = "Required";
   } else if (values.firstName.length < 2) {
-    errors.firstName = "Must be at least 2 characters.";
+    errors.firstName = "Must be at least 2 characters";
   } else if (values.firstName.length > 15) {
     errors.firstName = "Must be 15 characters or less";
   }
@@ -12,18 +12,18 @@ export const validateContactForm = (values) => {
   if (!values.lastName) {
     errors.lastName = "Required";
   } else if (values.lastName.length < 2) {
-    errors.lastName = "Must be at least 2 characters.";
+    errors.lastName = "Must be at least 2 characters";
   } else if (values.lastName.length > 15) {
     errors.lastName = "Must be 15 characters or less";
   }
 
   const reg = /^\d+$/;
   if (!reg.test(values.phoneNum)) {
-    errors.phoneNum = "The phone number should contain only numbers.";
+    errors.phoneNum = "The phone number should contain only numbers";
   }
 
   if (!values.email.includes("@")) {
-    errors.email = "Email should contain a @.";
+    errors.email = "Email should contain a @";
   }
 
   return errors;
